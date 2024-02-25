a019f83f89276549ff16fb2db648c5f23f660369
Amr AbdulSalam <amrabdulsalam@Firas-Dweikat.local>
2024-02-25 15:38:19 +0200

    Add Routing between pages

--- Frontend/invoice-tracker/src/App.js
+++ Frontend/invoice-tracker/src/App.js
@@ -1,70 +1,16 @@
 import React, { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Signin from './Signin';
+import Signup from './Signup';
 
 function App() {
-
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [emailError, setEmailError] = useState('')
-  const [passwordError, setPasswordError] = useState('')
-
-  const onButtonClick = () => {
-    setEmailError('')
-    setPasswordError('')
-  
-    // Check if the user has entered both fields correctly
-    if ('' === email) {
-      setEmailError('Please enter your email')
-      return
-    }
-  
-    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-      setEmailError('Please enter a valid email')
-      return
-    }
-  
-    if ('' === password) {
-      setPasswordError('Please enter a password')
-      return
-    }
-  
-    if (password.length < 7) {
-      setPasswordError('The password must be 8 characters or longer')
-      return
-    }
-  
-    alert(email+"\n"+password)
-  }
-
   return (
-    <div className={'mainContainer'}>
-    <div className={'titleContainer'}>
-      <div>Invoice Tracker</div>
-    </div>
-    <br />
-    <div className={'inputContainer'}>
-      <input
-        value={email}
-        placeholder="Enter your email here"
-        onChange={(ev) => setEmail(ev.target.value)}
-        className={'inputBox'}
-      />
-      <label className="errorLabel">{emailError}</label>
-    </div>
-    <br />
-    <div className={'inputContainer'}>
-      <input
-        value={password}
-        placeholder="Enter your password here"
-        onChange={(ev) => setPassword(ev.target.value)}
-        className={'inputBox'}
-      />
-      <label className="errorLabel">{passwordError}</label>
-    </div>
-    <br />
-    <div className={'inputContainer'}>
-      <input className={'inputButton'} type="button" onClick={onButtonClick} value={'Log in'} />
-    </div>
+    <div className="App">
+    <Routes>
+      <Route path="/" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   </div>
   );
 }
