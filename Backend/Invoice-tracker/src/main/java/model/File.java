0144dc795674459feb70792f373ef1d1fe2ac112
Amr AbdulSalam <amrabdulsalam@Firas-Dweikat.local>
2024-02-27 16:44:11 +0200

    Creating file model

---
+++ Backend/Invoice-tracker/src/main/java/model/File.java
@@ -0,0 +1,35 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "file")
+public class File implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "file_name", nullable = false)
+    private String fileName;
+
+    @Column(name = "file_path", nullable = false)
+    private String filePath;
+
+    @CreationTimestamp
+    @Column(name = "upload_date")
+    private Date uploadDate;
+
+    @Override
+    public String toString() {
+        return "File Name: " + this.fileName + " , " + "File Path: " + this.filePath + " , " +
+                "Upload Date: " + this.uploadDate;
+    }
+
+}
