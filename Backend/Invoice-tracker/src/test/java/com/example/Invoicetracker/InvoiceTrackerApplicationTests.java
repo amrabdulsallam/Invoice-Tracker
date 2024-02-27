7c4a5e452ce82686b9401a2bffca6e79e004ba6e
Amr AbdulSalam <amrabdulsalam@Firas-Dweikat.local>
2024-02-27 11:06:58 +0200

    Init spring boot project

---
+++ Backend/Invoice-tracker/src/test/java/com/example/Invoicetracker/InvoiceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Invoicetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
