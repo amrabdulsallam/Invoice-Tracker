dc66944b226e51679e733c8bdc5272de9a957974
Amr AbdulSalam <amrabdulsalam@Firas-Dweikat.local>
2024-03-10 14:05:59 +0200

    Adding invoices contnent dashboard endpoint with page size 3

--- Frontend/invoice-tracker/src/Dashboard.js
+++ Frontend/invoice-tracker/src/Dashboard.js
@@ -1,11 +1,49 @@
-import FileView from "./FileViewComponenet";
 import './App.css';
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import jwt_decode, { jwtDecode } from 'jwt-decode';
+import InvoicesList from './InvoicesList';
 
 const Dashboard = () => {
     const [page , setPage] = useState(0)
+    const [userId ,setUserId] = useState();
+    const [invoices , setInvoices] = useState([])
+    
+    useEffect(()=> {
+        const token = localStorage.getItem('token')
+        if(token === null){
+            window.location.href = '/';
+        }
+        const decodedToken = jwtDecode(token)
+        console.log(decodedToken)
+        setUserId(decodedToken.UserId);
+        const currentTimestamp = Math.floor(Date.now() / 1000);
+        if (decodedToken.exp < currentTimestamp) {
+            window.location.href = '/';
+        }
 
+        const fetchData = async (e) =>{
+            try {
+                const response = await axios.get('http://localhost:8080/api/v1/users/'+userId+'/invoices',
+                {
+                    params : {
+                        page : page,
+                        size : 3
+                    },
+                    headers: {
+                        'Authorization': 'Bearer '+localStorage.getItem('token'),
+                        'Content-Type': 'application/json'
+                      }
+                });
+                setInvoices(response.data.content)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchData();
+        
+    },[userId,page])
     const HandlePrePage = () => {
         if(page >= 1){
             setPage(page - 1);
@@ -16,27 +54,10 @@ const Dashboard = () => {
         setPage(page + 1)
     }
 
-    const files = [ {
-        fileName: 'file1.txt', 
-        uploadDate: '2005', 
-        updateDate : '2010'
-          
-      },
-      {
-        fileName: 'file10.txt',
-        uploadDate: '2040', 
-        updateDate : '2020'
-      },
-      ];
     return(
         <div>
             <h1>Dashboard</h1>
-            {files.map(function(data) {
-            return (
-                <FileView fileName={data.fileName} uploadDate={data.uploadDate} updateDate={data.updateDate}/>
-            )
-            })}
-
+            <InvoicesList invoices={invoices} />
             <footer className="pages">
                 <button onClick={HandlePrePage}>Prev</button>
                 <p>{page}</p>
