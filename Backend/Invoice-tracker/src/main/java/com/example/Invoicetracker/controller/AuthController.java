b3d2c6154afaddd77dbbdd10c2e6eebbde5cc871
Amr AbdulSalam <amrabdulsalam@Firas-Dweikat.local>
2024-03-11 11:59:00 +0200

    Adding logging message in the controllers

--- Backend/Invoice-tracker/src/main/java/com/example/Invoicetracker/controller/AuthController.java
+++ Backend/Invoice-tracker/src/main/java/com/example/Invoicetracker/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.example.Invoicetracker.controller;
 import com.example.Invoicetracker.model.User;
 import com.example.Invoicetracker.service.UserService;
 import com.example.Invoicetracker.service.dto.UserLoginDTO;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1")
 public class AuthController {
 
+    Logger logger = LoggerFactory.getLogger(AuthController.class);
+
     private final UserService userService;
 
     @Autowired
@@ -25,15 +29,19 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<?> checkUserCredentials(@RequestBody UserLoginDTO user) {
         try {
+            logger.info("Attempting to log in user : " + user.getEmail());
             String token = userService.checkCredentials(user);
+            logger.info("User have successfully logged in : " + user.getEmail());
             return ResponseEntity.status(HttpStatus.OK).body(token);
         } catch (Exception e) {
+            logger.error("Error while getting the token : {} ",e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("No user found");
         }
     }
 
     @PostMapping("signup")
     public ResponseEntity<?> saveUser(@RequestBody User user) {
+        logger.info("Attempting to sign up with info : " + user);
         return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
     }
 
--- Backend/Invoice-tracker/src/main/java/com/example/Invoicetracker/controller/FileController.java
+++ Backend/Invoice-tracker/src/main/java/com/example/Invoicetracker/controller/FileController.java
@@ -3,6 +3,8 @@ package com.example.Invoicetracker.controller;
 import com.example.Invoicetracker.service.FileService;
 import com.example.Invoicetracker.service.dto.FileDTO;
 import com.example.Invoicetracker.service.dto.FileReturnDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/files")
 public class FileController {
+
+    Logger logger = LoggerFactory.getLogger(FileController.class);
 
     private final FileService fileService;
 
@@ -25,23 +29,29 @@ public class FileController {
     @PostMapping()
     public ResponseEntity<?> saveFile(@ModelAttribute FileDTO file) {
         try {
+            logger.info("File uploaded with info : " + file);
             return ResponseEntity.status(HttpStatus.CREATED).body(fileService.saveFile(file));
         } catch (Exception e) {
+            logger.error("Failed to upload file : {} ", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed");
         }
     }
 
     @GetMapping()
     public ResponseEntity<List<FileReturnDTO>> getAllFiles() {
+        logger.info("Attempt to get all files");
         return ResponseEntity.status(HttpStatus.OK).body(fileService.getFiles());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getFileById(@PathVariable long id) {
         try {
+            logger.info("Attempt to get all file with id " + id);
             FileReturnDTO file = fileService.getFileById(id);
+            logger.info("Successful getting file with id " + id);
             return ResponseEntity.status(HttpStatus.OK).body(file);
         } catch (Exception e) {
+            logger.error("Failed to get file : {} ", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("No file found");
         }
     }
@@ -49,8 +59,10 @@ public class FileController {
     @PutMapping("/{id}")
     public ResponseEntity<?> updateFile(@ModelAttribute FileDTO newFile, @PathVariable long id) {
         try {
+            logger.info("Attempt to update file with id " + id);
             return ResponseEntity.status(HttpStatus.OK).body(fileService.updateFile(newFile, id));
         } catch (Exception e) {
+            logger.error("Failed to update file : {} ", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("No file found");
         }
     }
@@ -58,9 +70,12 @@ public class FileController {
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteFile(@PathVariable long id) {
         try {
+            logger.info("Attempt to delete file with id " + id);
             fileService.deleteFile(id);
+            logger.info("file deleted with id " + id);
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         } catch (Exception e) {
+            logger.error("Failed to delete file : {} ", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("No file found");
         }
     }
