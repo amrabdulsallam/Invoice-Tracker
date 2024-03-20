d3abe55762c782f79b4339ef4617ad5ada54b9c8
Amr AbdulSalam <amrabdulsalam@Firas-Dweikat.local>
2024-03-20 09:39:43 +0200

    Refactor controller and remove try-catch blocks

--- Backend/Invoice-tracker/src/main/java/com/example/Invoicetracker/controller/InvoiceController.java
+++ Backend/Invoice-tracker/src/main/java/com/example/Invoicetracker/controller/InvoiceController.java
@@ -1,12 +1,11 @@
 package com.example.Invoicetracker.controller;
 
+import com.example.Invoicetracker.controller.entity.ResponseEntity;
 import com.example.Invoicetracker.service.InvoiceService;
 import com.example.Invoicetracker.service.dto.InvoiceDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,57 +25,37 @@ public class InvoiceController {
 
     @PostMapping
     public ResponseEntity<?> saveInvoice(@RequestBody InvoiceDTO invoice) {
-        try {
-            logger.info("Attempt to save invoice with info : " + invoice);
-            return ResponseEntity.status(HttpStatus.CREATED).body(invoiceService.saveInvoice(invoice));
-        } catch (Exception e) {
-            logger.error("Failed to save invoice : {} ", e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Invoice failed to upload");
-        }
+        logger.info("Attempt to save invoice with info : " + invoice);
+        return new ResponseEntity<>(invoiceService.saveInvoice(invoice));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getInvoiceById(@PathVariable long id) {
-        try {
-            logger.info("Attempt to get invoice with id : " + id);
-            return ResponseEntity.status(HttpStatus.OK).body(invoiceService.getInvoiceById(id));
-        } catch (Exception e) {
-            logger.error("Failed to get invoice : {} ", e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Invoice not found for Id " + id);
-        }
+        logger.info("Attempt to get invoice with id : " + id);
+        return new ResponseEntity<>(invoiceService.getInvoiceById(id));
     }
 
     @GetMapping
     @PreAuthorize("hasAnyRole('SUPER_USER', 'AUDITOR')")
     public ResponseEntity<?> getAllInvoices() {
         logger.info("Attempt to get all invoices");
-        return ResponseEntity.status(HttpStatus.OK).body(invoiceService.getInvoices());
+        return new ResponseEntity<>(invoiceService.getInvoices());
     }
 
     @PutMapping("/{id}")
     @PreAuthorize("hasAnyRole('SUPER_USER', 'USER')")
     public ResponseEntity<?> updateInvoice(@RequestBody InvoiceDTO invoice, @PathVariable long id) {
-        try {
-            logger.info("Attempt to update invoice with id : " + id);
-            return ResponseEntity.status(HttpStatus.OK).body(invoiceService.updateInvoice(invoice, id));
-        } catch (Exception e) {
-            logger.error("Failed to update invoice : {} ", e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Invoice didn't update");
-        }
+        logger.info("Attempt to update invoice with id : " + id);
+        return new ResponseEntity<>(invoiceService.updateInvoice(invoice, id));
     }
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAnyRole('SUPER_USER', 'USER')")
     public ResponseEntity<?> deleteInvoice(@PathVariable long id) {
-        try {
-            logger.info("Attempt to delete invoice with id : " + id);
-            invoiceService.deleteInvoice(id);
-            logger.info("Invoice deleted with id : " + id);
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-        } catch (Exception e) {
-            logger.error("Failed to delete invoice : {} ", e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Invoice cannot be deleted");
-        }
+        logger.info("Attempt to delete invoice with id : " + id);
+        invoiceService.deleteInvoice(id);
+        logger.info("Invoice deleted with id : " + id);
+        return new ResponseEntity<>();
     }
 
 }
--- Backend/Invoice-tracker/src/main/java/com/example/Invoicetracker/controller/UserController.java
+++ Backend/Invoice-tracker/src/main/java/com/example/Invoicetracker/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.example.Invoicetracker.controller;
 
-import com.example.Invoicetracker.exception.UserNotFoundException;
+import com.example.Invoicetracker.controller.entity.ResponseEntity;
 import com.example.Invoicetracker.model.User;
 import com.example.Invoicetracker.service.UserService;
 import com.example.Invoicetracker.service.dto.InvoiceReturnDTO;
@@ -11,8 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,56 +33,41 @@ public class UserController {
     @PostMapping()
     public ResponseEntity<?> saveUser(@RequestBody User user) {
         logger.info("A new user added with email : " + user.getEmail());
-        return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
+        return new ResponseEntity<>(userService.saveUser(user));
     }
 
     @GetMapping()
     @PreAuthorize("hasRole('SUPER_USER')")
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         logger.info("Getting all users");
-        return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
+        return new ResponseEntity<>(userService.getUsers());
     }
 
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('SUPER_USER')")
     public ResponseEntity<?> getUserById(@PathVariable long id) {
-        try {
-            logger.info("Attempt to get user with id : " + id);
-            UserDTO user = userService.getUserById(id);
-            logger.info("Successful getting user with id " + id);
-            return new ResponseEntity<>(user, HttpStatus.OK);
-        } catch (UserNotFoundException e) {
-            logger.error("Failed to save user : {} ", e.getMessage());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
+        logger.info("Attempt to get user with id : " + id);
+        UserDTO user = userService.getUserById(id);
+        logger.info("Successful getting user with id " + id);
+        return new ResponseEntity<>(user);
     }
 
     @PutMapping("/{id}")
     @PreAuthorize("hasAnyRole('SUPER_USER', 'USER')")
     public ResponseEntity<?> updateUser(@RequestBody User newUser, @PathVariable long id) {
-        try {
-            logger.info("Attempt to update user with id : " + id);
-            UserDTO user = userService.updateUser(newUser, id);
-            logger.info("Successful user updated with id " + id);
-            return new ResponseEntity<>(user, HttpStatus.OK);
-        } catch (UserNotFoundException e) {
-            logger.error("Failed to update user : {} ", e.getMessage());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
+        logger.info("Attempt to update user with id : " + id);
+        UserDTO user = userService.updateUser(newUser, id);
+        logger.info("Successful user updated with id " + id);
+        return new ResponseEntity<>(user);
     }
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('SUPER_USER')")
     public ResponseEntity<?> deleteUserById(@PathVariable long id) {
-        try {
-            logger.info("Attempt to delete user with id : " + id);
-            userService.deleteUser(id);
-            logger.info("Successful user deleted with id " + id);
-            return ResponseEntity.noContent().build();
-        } catch (UserNotFoundException e) {
-            logger.error("Failed to delete user : {} ", e.getMessage());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
+        logger.info("Attempt to delete user with id : " + id);
+        userService.deleteUser(id);
+        logger.info("Successful user deleted with id " + id);
+        return new ResponseEntity<>();
     }
 
     @GetMapping("/{id}/invoices")
@@ -93,21 +76,16 @@ public class UserController {
                                                   @RequestParam(defaultValue = "0") int page,
                                                   @RequestParam(defaultValue = "3") int size,
                                                   @RequestParam(name = "search", required = false) String search) {
-        try {
-            logger.info("Attempt to get all invoices by user with id : " + id);
-            Pageable pageable = PageRequest.of(page, size);
-            if (search != null && !search.isEmpty()) {
-                Page<InvoiceReturnDTO> invoices = userService.getAllInvoicesByUserWithSearch(id, pageable, search);
-                logger.info("Successful getting all invoices by user with {} and search {}", id, search);
-                return ResponseEntity.status(HttpStatus.OK).body(invoices);
-            } else {
-                Page<InvoiceReturnDTO> invoices = userService.getAllInvoicesByUser(id, pageable);
-                logger.info("Successful getting all invoices by user with id " + id);
-                return ResponseEntity.status(HttpStatus.OK).body(invoices);
-            }
-        } catch (Exception e) {
-            logger.error("Failed to get user invoices : {} ", e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        logger.info("Attempt to get all invoices by user with id : " + id);
+        Pageable pageable = PageRequest.of(page, size);
+        if (search != null && !search.isEmpty()) {
+            Page<InvoiceReturnDTO> invoices = userService.getAllInvoicesByUserWithSearch(id, pageable, search);
+            logger.info("Successful getting all invoices by user with {} and search {}", id, search);
+            return new ResponseEntity<>(invoices);
+        } else {
+            Page<InvoiceReturnDTO> invoices = userService.getAllInvoicesByUser(id, pageable);
+            logger.info("Successful getting all invoices by user with id " + id);
+            return new ResponseEntity<>(invoices);
         }
     }
 
